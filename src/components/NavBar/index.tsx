ceb7ca08e32900d2ca3b017eb0621ce4824c22b9
Lou Carther <dieudrichard@gmail.com>
2023-12-14 21:19:44 +0100

    style(app): Add theme color mode switch

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,16 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "@assets/logo.webp";
 import ConfigApp from "@config/app";
+import { ColorModeSwitch } from "@components/index";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={"60px"} />
-      <Text>{ConfigApp.app_name}</Text>
+    <HStack justifyContent={"space-between"}>
+      <HStack>
+        <Image src={logo} boxSize={"60px"} />
+        <Text>{ConfigApp.app_name}</Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
