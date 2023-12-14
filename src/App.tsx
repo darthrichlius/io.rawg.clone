caa762d30c1c2547a3e2a15eae69b4e49e2b08ff
Lou Carther <dieudrichard@gmail.com>
2023-12-14 20:29:39 +0100

    chore(app): Build an initial responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Button</Button>
-    </>
+    /** @see https://chakra-ui.com/docs/styled-system/responsive-styles */
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // We are responsive-first
+        lg: `"nav nav" "aside main"`, //min: 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
