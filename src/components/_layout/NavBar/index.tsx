ad93737b6014e750417cd1d4a1985651476fea27
Lou Carther <dieudrichard@gmail.com>
2023-12-30 22:15:43 +0100

    feat(NavBar): Redirect to GameList on Search

--- src/components/_layout/NavBar/index.tsx
+++ src/components/_layout/NavBar/index.tsx
@@ -3,12 +3,18 @@ import logo from "@assets/logo.webp";
 import ConfigApp from "@config/app";
 import { ColorModeSwitch, SearchInput } from "@/components";
 import { useGameQueryStore } from "@/stores";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const { onSearch } = useGameQueryStore((s) => ({
     onSearch: s.setSearch,
   }));
+  const navigate = useNavigate();
+
+  const handleSearch = (t: string) => {
+    onSearch(t);
+    navigate("/");
+  };
 
   return (
     <Grid
@@ -19,13 +25,15 @@ const NavBar = () => {
       paddingY={10}
     >
       <HStack>
-        <Image src={logo} boxSize={"60px"} />
+        <Link to="/">
+          <Image src={logo} boxSize={"50px"} />
+        </Link>
         <Link to="/">
           <Text>{ConfigApp.app_name}</Text>
         </Link>
       </HStack>
       <HStack justifyContent={"space-between"} paddingX={10}>
-        <SearchInput onSearch={onSearch} placeholder="Search Games" />
+        <SearchInput onSearch={handleSearch} placeholder="Search Games" />
         <ColorModeSwitch />
       </HStack>
     </Grid>
